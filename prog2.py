01fd3ea6940583afdab758c1ab601f34319196c5
Komal Chaudhary <jaatkomal9876@gmail.com>
2024-04-30 18:57:17 +0530

    change

--- prog2.py
+++ prog2.py
@@ -7,8 +7,16 @@ if(a>b):
   print(1)
   pass
 print(10)
-for i in range (0,5):
+for i in range (0,100,2):
   if(i==2 or i==5):
     pass
   else:
-    print("Ha fool!")
+    print("Ha fool!" ,end=" ")
+print(14%12//14)
+print(int(13.25+4/2))
+print(8/4/2)
+print(float('12'))
+print("hello\\example\\test.txt")
+print('''EINA
+mina
+dika''')
