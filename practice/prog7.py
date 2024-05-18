6d6bb4c4a98aed42ceb1d7c574513f782bf8c616
Komal Chaudhary <jaatkomal9876@gmail.com>
2024-05-18 23:58:53 +0530

    update

---
+++ practice/prog7.py
@@ -0,0 +1,15 @@
+# tempconversion.py
+"""converting f and c"""
+# Functions
+def fah(c):
+  """return temp in f"""
+  return 9*c/5.0+32
+
+def cel(f):
+  """return temp in c"""
+  return 5*(32-f)/9.0
+
+# Constants
+frec=0.0  #water freezing temp in c
+fref=32.0 #water freezing temp in f
+
