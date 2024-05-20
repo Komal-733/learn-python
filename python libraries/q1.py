917ea1c10892e9836443bf9bbae8da894a53fb76
Komal Chaudhary <jaatkomal9876@gmail.com>
2024-05-20 22:08:49 +0530

    update

--- python libraries/q1.py
+++ python libraries/q1.py
@@ -1,5 +1,14 @@
+import string
 def remove_letter(sentence,letter):
   sen=sentence.split(letter)
-  for i in sen:
-    print(i,end='')
+  print("".join(sen))
 remove_letter('hello there!','e')
+
+def func(s):
+  print(string.capwords(s,'h'))
+  sen=s.split('h')
+  for i in sen:
+    sen[sen.index(i)]=i.capitalize()
+  sen="".join(sen)
+  print(sen)
+func("hello there!")
