5810a0937999132131b9cbd4876921956248c466
Komal Chaudhary <jaatkomal9876@gmail.com>
2024-05-21 17:54:58 +0530

    update

--- python libraries/lengthconversion.py
+++ python libraries/lengthconversion.py
@@ -3,4 +3,26 @@
 
 def miletokm(mile):
   '''convert length mile to km'''
-  km=
+  km=mile*1.609344
+  return km
+
+def kmtomile(km):
+  '''convert length km to mile'''
+  mile=km/1.609344
+  return mile
+
+def feettoinches(feet):
+  '''convert length feet to inches'''
+  inch=12*feet
+  return inch
+
+def inchestofeet(inch):
+  '''convert length inches to feet'''
+  feet=inch/12
+  return feet
+
+#constants
+km=1.609344
+mile=1
+feet=1
+inch=12
---
+++ python libraries/q2lengthconversion.py
@@ -0,0 +1,2 @@
+import lengthconversion
+help(lengthconversion)
