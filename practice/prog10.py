032267899f1f5afc92cb5b065447cd591fb0f8f6
Komal Chaudhary <jaatkomal9876@gmail.com>
2024-05-23 18:01:35 +0530

    update

--- practice/prog10.py
+++ practice/prog10.py
@@ -1,5 +1,4 @@
 import sys
-file=open("secondfile.txt","a+")
-l1=file.write("hi\n")
-sys.stdin.read()
+file=open("practice\secondfile.txt","a+")
+file.write("hi\n")
 file.close()
--- practice/prog9.py
+++ practice/prog9.py
@@ -1,6 +1,4 @@
-myfile=open("firstfile.txt","r+")
-print(myfile.read())
+myfile=open("practice\\firstfile.txt","a+")
 myfile.write("hello world"+"\t")
 myfile.flush()
-
 myfile.close()
